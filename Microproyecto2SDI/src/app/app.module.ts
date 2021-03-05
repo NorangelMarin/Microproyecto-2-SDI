3267bc998df4557c116950d8d6d343df77e818e1
Jenderson Quintero <j.quintero@correo.unimet.edu.ve>
2021-03-05 18:35:39 -0400

    Conexion con Firebase

--- Microproyecto2SDI/src/app/app.module.ts
+++ Microproyecto2SDI/src/app/app.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
+// Modulos de Firebase
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+import { AngularFireStorageModule } from "@angular/fire/storage";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+
+// Import de Environment
+import { environment } from "src/environments/environment"
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -26,7 +35,12 @@ import { BtnFavoritosComponent } from './components/btn-favoritos/btn-favoritos.
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Microproyecto2SDI/src/environments/environment.ts
+++ Microproyecto2SDI/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBDGYbKP5EhUL_1ILiztVYHFfCZtp0CDV0",
+    authDomain: "microproyecto-2-sdi.firebaseapp.com",
+    projectId: "microproyecto-2-sdi",
+    storageBucket: "microproyecto-2-sdi.appspot.com",
+    messagingSenderId: "262909958696",
+    appId: "1:262909958696:web:ad44dd8b8519074ac0b31d"
+  }
 };
 
 /*
