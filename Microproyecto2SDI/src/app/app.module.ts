77dc18313e43db404be248761421ae4acd392c93
Norangel Marin <norangel.marin@correo.unimet.edu.ve>
2021-03-05 16:53:42 -0400

    primeras cositas del diseño principal

--- Microproyecto2SDI/src/app/app.module.ts
+++ Microproyecto2SDI/src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PeliculasComponent } from './components/peliculas/peliculas.component';
+import { ListaPeliculasComponent } from './components/lista-peliculas/lista-peliculas.component';
+import { DetallesPeliculaComponent } from './components/detalles-pelicula/detalles-pelicula.component';
+import { ReservaComponent } from './components/reserva/reserva.component';
+import { FavoritosComponent } from './components/favoritos/favoritos.component';
+import { BtnFavoritosComponent } from './components/btn-favoritos/btn-favoritos.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    InicioComponent,
+    LoginComponent,
+    FooterComponent,
+    NavbarComponent,
+    PeliculasComponent,
+    ListaPeliculasComponent,
+    DetallesPeliculaComponent,
+    ReservaComponent,
+    FavoritosComponent,
+    BtnFavoritosComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
