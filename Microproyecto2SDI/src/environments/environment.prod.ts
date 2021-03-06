a76bf3d5021d9b3116e9039f7242f6c4a4dd9f93
JendersonQuinter <j.quintero@correo.unimet.edu.ve>
2021-03-05 23:19:50 -0400

    Correcion de error para conexion con Firebase

--- Microproyecto2SDI/src/environments/environment.prod.ts
+++ Microproyecto2SDI/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBDGYbKP5EhUL_1ILiztVYHFfCZtp0CDV0",
+    authDomain: "microproyecto-2-sdi.firebaseapp.com",
+    projectId: "microproyecto-2-sdi",
+    storageBucket: "microproyecto-2-sdi.appspot.com",
+    messagingSenderId: "262909958696",
+    appId: "1:262909958696:web:ad44dd8b8519074ac0b31d"
+  }
 };
